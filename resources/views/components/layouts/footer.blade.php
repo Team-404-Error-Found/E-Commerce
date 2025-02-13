2abc9b25f487f6bc7f5463a57f3743d62f017c69
saranR <720721106096@hicet.ac.in>
2025-02-13 21:46:17 +0530

    using footer and header as a seperate file inside the components/layouts -saran

---
+++ resources/views/components/layouts/footer.blade.php
@@ -0,0 +1,80 @@
+<footer class="bg-black text-white p-4 mt-5">
+    <div class="grid text-start grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-y-6 gap-x-2 text-sm">
+        <div class="block">
+            <p class="font-bold mb-3">ABOUT</p>
+            <li><a href="">Contact Us</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Careers</a></li>
+            <li><a href="">Flipkart Stories</a></li>
+            <li><a href="">Press</a></li>
+            <li><a href="">Corporate Information</a></li>
+        </div>
+        <div>
+            <p class="font-bold mb-3">GROUP COMPANIES</p>
+            <li><a href="">Myntra</a></li>
+            <li><a href="">Cleartrip</a></li>
+            <li><a href="">Shopsy</a></li>
+        </div>
+        <div>
+            <p class="font-bold mb-3">GROUP COMPANIES</p>
+            <li><a href="">Payments</a></li>
+            <li><a href="">Shipping</a></li>
+            <li><a href="">Cancellation & Returns</a></li>
+            <li><a href="">FAQ</a></li>
+
+        </div>
+        <div>
+            <p class="font-bold mb-3">HELP</p>
+            <li><a href="">Cancellation & Return</a></li>
+            <li><a href="">Terms Of Use</a></li>
+            <li><a href="">Security</a></li>
+            <li><a href="">Privacy</a></li>
+            <li><a href="">Sitemap</a></li>
+            <li><a href="">Grievance Redressal</a></li>
+            <li><a href=""> EPR Compliance</a></li>
+        </div>
+        <div>
+            <p class="font-bold">Mail Us</p>
+            <p class="mt-3">Flipkart Internet Private Limited Buildings Alyssa, Begonía 8 Clove Embassy Tech Vllage, Outer Ring Road, Devarabeesanahali village, Bengaluru, 560103, Karnataka, India</p>
+            <p class="my-2">Social</p>
+            <div class="flex justify-content-start gap-x-5">
+                <img src="{{asset('images/instagram.png')}}" width="20px" height="20px" alt="instagram logo">
+                <img src="{{asset('images/facebook.png')}}" width="20px" height="20px" alt="facebook logo">
+                <img src="{{asset('images/twitter.png')}}" width="20px" height="20px" alt="twitter logo">
+            </div>
+        </div>
+        <div>
+            <p class="font-bold">Registered Office Address</p>
+            <p class="mt-3">Flipkart Internet Private Limited Buildings Alyssa, Begonia & Clove Embassy Tech Village Outer Ring Road, Devarabeesanahalli Village Bengaluru, 560103 Karnataka, India CIN:U51109KA2012PTCO66107 Telephone: 044-45614700/044-67415800</p>
+        </div>
+    </div>
+    <hr/>
+    <div class="block md:flex justify-content-center text-sm gap-x-2">
+        <div class="flex my-2 gap-x-3">
+            <img src="{{asset('images/best-seller.png')}}" width="20px" height="20px">
+            <p class="flex items-center">Become a Seller</p>
+        </div>
+        <div class="flex my-2 gap-x-3">
+            <img src="{{asset('images/ads.png')}}" width="20px" height="20px">
+            <p class="flex items-center">Advertise</p>
+        </div>
+        <div class="flex my-2 gap-x-3">
+            <img src="{{asset('images/gift-card.png')}}" width="20px" height="20px">
+            <p class="flex items-center">Gift Cards</p>
+        </div>
+        <div class="flex my-2 gap-x-3">
+            <img src="{{asset('images/customer care.png')}}" width="20px" height="20px">
+            <p class="flex items-center">Help Center</p>
+        </div>
+        <p class="flex items-center">&copy; 2007-2025 Flipkart.com</p>
+        <div class="flex my-2 gap-x-2">
+            <img src="{{asset('images/card1.png')}}" width="30px" height="30px">
+            <img src="{{asset('images/card2.png')}}" width="30px" height="30px">
+            <img src="{{asset('images/card3.png')}}" width="30px" height="30px">
+            <img src="{{asset('images/card4.png')}}" width="30px" height="30px">
+            <img src="{{asset('images/card5.png')}}" width="30px" height="30px">
+            <img src="{{asset('images/card6.png')}}" width="30px" height="30px">
+            <img src="{{asset('images/card7.png')}}" width="30px" height="30px">
+        </div>
+    </div>
+</footer>
---
+++ resources/views/components/layouts/header.blade.php
@@ -0,0 +1,23 @@
+<header class="flex justify-between items-center p-4">
+    <div class="flex">
+        {{--            <img src="{{ asset('images/menu.png') }}" width="30px" height="30px" alt="menu">--}}
+        <h2 class="ml-2 md:text-lg lg:text-xl">Flipkart Clone</h2>
+    </div>
+    <div>
+        <ul class="flex text-xl space-x-4  lg:space-x-8">
+            <input type="text" placeholder="Search for Products, Brands and More" class="rounded-l-md p-2 text-md md:text-sm lg:text-md hidden sm:hidden md:block lg:block md:w-[400px] lg:w-[600px] bg-blue-100">
+            <div class="flex">
+                <img width="40px" height="40px" src="{{ asset('images/login img.png') }}" class=" p-2 rounded-lg" alt="loginimg">
+                <p class="flex justify-center items-center">Login</p>
+            </div>
+            <div class="flex" onclick="location.href='{{route('cart')}}'">
+                <img src="{{ asset('images/cart.png') }}" width="40px" height="30px" class="p-2 lg:text-2xl"  alt="cart">
+                <p class="justify-center items-center hidden lg:flex">Cart</p>
+            </div>
+            <div class="flex">
+                <img src="{{ asset('images/seller.png') }}" width="40px" height="40px" class="p-2 lg:text-2xl" alt="seller">
+                <p class="justify-center items-center hidden lg:flex ">Become a Seller</p>
+            </div>
+        </ul>
+    </div>
+</header>
